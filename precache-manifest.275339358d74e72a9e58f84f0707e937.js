924a4f5952962242c47a7da4f00d7d0b3dd592b7
ritrubia <r.trubiani@gmail.com>
2021-12-12 12:53:11 +0100

    Updates

---
+++ precache-manifest.275339358d74e72a9e58f84f0707e937.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8b70ffda2dd7be67dca1aff4ff73d13",
+    "url": "https//insuredapp.github.io/client/index.html"
+  },
+  {
+    "revision": "a3448541495cb4e6da6e",
+    "url": "https//insuredapp.github.io/client/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "95c9b772a8f593dc7287",
+    "url": "https//insuredapp.github.io/client/static/js/2.95fa6c05.chunk.js"
+  },
+  {
+    "revision": "a3448541495cb4e6da6e",
+    "url": "https//insuredapp.github.io/client/static/js/main.f5121438.chunk.js"
+  },
+  {
+    "revision": "f3cb85889f63986ba18d",
+    "url": "https//insuredapp.github.io/client/static/js/runtime-main.a28c94fe.js"
+  }
+]);
